3c34023065a119316b093e7ffcb6dcf97d7df884
Spenser <spenser@172-29-241-132.dynapool.nyu.edu>
2019-11-11 20:30:50 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const recipeModel = require('./api/recipe.model');
+const recipeControllers = require('./api/recipe.controllers');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
+const dataBaseURL = process.env.DATABASE;
+  // 'mongodb+srv://daniel:dd2345@recipes-3k4ea.mongodb.net/test?retryWrites=true&w=majority';
+
+app.get('/api/recipes', recipeControllers.findAll);
+app.get('/api/recipes/:id', recipeControllers.findById);
+app.post('/api/recipes', recipeControllers.add);
+app.put('/api/recipes/:id', recipeControllers.update);
+app.delete('/api/recipes/:id', recipeControllers.delete);
+app.get('/api/import', recipeControllers.import);
+app.get('/api/killall', recipeControllers.killall);
+
+mongoose
+  .connect(dataBaseURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDb connected'))
+  .catch(err => console.log(err));
+
+// const PORT = 4000;
+// app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
